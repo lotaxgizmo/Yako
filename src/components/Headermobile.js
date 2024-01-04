dd2b3ff28c25d2a916a8e087150e0721828569ca
Lotachi <lotaoko2@gmail.com>
2024-01-04 17:25:10 +0100

    Holla

--- src/components/Headermobile.js
+++ src/components/Headermobile.js
@@ -8,24 +8,47 @@ import yakonormal from '../assets/yakonormal.svg'
 
 
 function Headermobile() {
+    const [isMenuOpen, setMenuOpen] = useState(false);
 
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
 
 
     return (
         <div className='lg:hidden fixed z-[200] flex flex-col items-center'>
 
-            <div><img src={hamburger} alt="" className='  w-[70px] z-[200] border-8s mt-2' /></div>
-            {/* <div className='relative flex flex-col items-center justify-center text-black'>
-                <img src={menuopen} alt="" className='mt-5  ' />
-                <div className='absolute top-8'><img src={closehamburger} alt="" className='  w-[60px] z-[200] border-8s mt-2d' /></div>
-                <div className='absolute top-24'><img src={yakonormal} alt="" className='  w-[76px] z-[200] border-8s mt-2d' /></div>
-                <div className="links flex flex-col absolute bottom-10 border-4d z-40">
-                    <a href="#Home" className='md: text-[53px] leading-[60px] py-2 z-40 border-b-2 border-black w-64- text-center'>Home</a>
-                    <a href="#About" className='md: text-[53px] leading-[60px] py-2 z-40 border-b-2 border-black w-64- text-center'>About</a>
-                    <a href="https://t.me/yakosolana" className='md: text-[53px] leading-[60px] py-2 z-40 border-b-2 border-black w-64- text-center'>Telegram</a>
-                    <a href="#Raydium" className='md: text-[53px] leading-[60px] py-2 z-40 border-b-2 border-black w-64- text-center'>Raydium</a>
-                </div>
-            </div> */}
+            <div>
+                <button
+                    className="lg:hidden"
+                    onClick={toggleMenu}
+                >
+                    {isMenuOpen ? '' : <div><img src={hamburger} alt="" className='  w-[70px] z-[200] border-8s mt-2' /></div>}
+                </button>
+
+                {isMenuOpen && (
+                    <div className="lg:hidden">
+                        {/* Your full menu content goes here */}
+                        <ul>
+                            <div className='relative flex flex-col items-center justify-center text-black'>
+                                <img src={menuopen} alt="" className='mt-5  ' />
+                                <div className='absolute top-8'><img src={closehamburger} alt="" className='  w-[60px] z-[200] border-8s mt-2d' onClick={toggleMenu} /></div>
+                                <div className='absolute top-24'><img src={yakonormal} alt="" className='  w-[76px] z-[200] border-8s mt-2d' /></div>
+                                <div className="links flex flex-col absolute bottom-10 border-4d z-40">
+                                    <a href="#Home" className='md: text-[50px] leading-[60px] py-2 z-40 border-b-2 border-black w-64- text-center'>Home</a>
+                                    <a href="#About" className='md: text-[50px] leading-[60px] py-2 z-40 border-b-2 border-black w-64- text-center'>About</a>
+                                    <a href="https://t.me/yakosolana" className='md: text-[50px] leading-[60px] py-2 z-40 border-b-2 border-black w-64- text-center'>Telegram</a>
+                                    <a href="#Raydium" className='md: text-[50px] leading-[60px] py-2 z-40 border-b-2 border-black w-64- text-center'>Raydium</a>
+                                </div>
+                            </div>
+                            {/* Add more menu items as needed */}
+                        </ul>
+                    </div>
+                )}
+            </div>
+
+
+
         </div>
     )
 }
